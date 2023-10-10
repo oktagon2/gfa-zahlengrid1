8915def63675a66a8d41ccd58e0fb32992ba6f26
Ruedi Schwarzenbach <ruedi.schwarzenbach@gmx.ch>
2023-10-10 07:23:04 +0200

    Transfer

--- js/index.js
+++ js/index.js
@@ -18,11 +18,23 @@ stepButton.onclick= function() {
         const row= document.createElement( 'tr');
         table.appendChild( row);
         const cell= document.createElement( 'td');
-        table.appendChild( cell);
+        row.appendChild( cell);
         const textNode= document.createTextNode( '1');
         cell.appendChild( textNode);
     }
     else {
+        const table= gridDiv.firstChild;
+        const lastRow= table.lastChild;
+        const lastCell= lastRow.lastChild;
+        if(lastRow.childNodes.length< widthInput.value) {
+            const cell= document.createElement( 'td');
+            row.appendChild( cell);
+            const textNode= document.createTextNode( lastCell.value+ 1);
+            cell.appendChild( textNode);
+        }
+        else {
+
+        }
     }
 }
 
